579607293413acb168324ed4f3788814075b6b70
Agil Adi Saputro <agiladisaputro@gmail.com>
2023-05-12 10:41:14 +0700

    [CREATE] new function to handle post comment request from user

--- router/router.go
+++ router/router.go
@@ -22,6 +22,10 @@ func StartServer(db *gorm.DB) *gin.Engine {
 	photoService := service.NewPhotoService(photoRepository, validate)
 	photoController := controller.NewPhotoController(photoService)
 
+	commentRepository := repository.NewCommentRepository(db)
+	commentService := service.NewCommentService(commentRepository, validate)
+	commentController := controller.NewCommentController(commentService)
+
 	app := gin.Default()
 
 	userRouter := app.Group("/users")
@@ -40,5 +44,10 @@ func StartServer(db *gorm.DB) *gin.Engine {
 		photoRouter.DELETE("/:id", photoController.DeletePhoto)
 	}
 
+	commentRouter := app.Group("/comments")
+	{
+		commentRouter.Use(middleware.JWTMiddleware())
+		commentRouter.POST("/", commentController.CreateComment)
+	}
 	return app
 }
