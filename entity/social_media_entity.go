e85f695517df536fde5c38facb9e64147ba8b9ee
Agil Adi Saputro <agiladisaputro@gmail.com>
2023-06-08 03:04:15 +0700

    [ADDED] new validation check on userId, SocialMedia Table

--- entity/social_media_entity.go
+++ entity/social_media_entity.go
@@ -8,7 +8,7 @@ type Socialmedia struct {
 	ID             uint   `gorm:"primaryKey"`
 	Name           string `gorm:"not null;type:varchar(30)" validate:"required"`
 	SocialMediaURL string `gorm:"not null;type:text" validate:"required"`
-	UserID         uint
+	UserID         uint   `gorm:"not null" validate:"required"`
 	CreatedAt      time.Time
 	UpdatedAt      time.Time
 }
